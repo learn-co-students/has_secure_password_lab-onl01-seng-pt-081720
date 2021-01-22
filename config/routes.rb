39c683a4fd845c4e9fcfdd9515797e573349b0ec
alexcoldbrew <alexcoldbrew@gmail.com>
2021-01-22 13:57:11 -0600

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
+  get 'users/new'
+
+  get 'users/create'
+
+  resources :sessions
+
+  get '/:id', to: 'users#welcome', as: 'welcome'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
