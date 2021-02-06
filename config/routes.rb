2cbb3005977a9e1a6aaba118994ba57995c3f70d
Janae Quinones <janaemassa@gmail.com>
2021-02-06 14:46:13 -0500

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :users
+  get "/sessions/new", to: "sessions#new"
+  post "/sessions", to: "sessions#create"
+  get "/homepage", to: "welcome#homepage"
+  
 end
